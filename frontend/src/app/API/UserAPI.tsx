ad252a3573e0eadc2c246e89b48797600a050518
hanseongeon <gkstjddjs08@naver.com>
2025-03-03 23:27:56 +0900

    과제1

--- frontend/src/app/API/UserAPI.tsx
+++ frontend/src/app/API/UserAPI.tsx
@@ -1,4 +1,13 @@
-import { headers } from 'next/headers';
 import { getAPI } from './AxiosAPI';
 
 export const UserApi = getAPI();
+
+interface orderRequsetDTO{
+    name: String;
+    count: Number;
+  }
+
+  export const sendOrder = async (data: orderRequsetDTO) => {
+    const response = await UserApi.post('/api/order', data);
+    return response.data;
+};
