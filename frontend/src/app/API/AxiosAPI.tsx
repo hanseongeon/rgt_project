7fa1ecd188497380c8f762ed025fab99de311bd2
hanseongeon <gkstjddjs08@naver.com>
2025-03-08 18:33:31 +0900

    배포

--- frontend/src/app/API/AxiosAPI.tsx
+++ frontend/src/app/API/AxiosAPI.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function getAPI() {
     const api = axios.create({
-        baseURL: 'http://13.125.86.24:8080',
+        baseURL: 'http://hserrgtproject.shop',
         headers: {
             'Content-Type': 'application/json; charset=utf-8',
         },
